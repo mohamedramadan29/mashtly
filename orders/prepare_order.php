85ac4aebbfa448aaec5ec27894e7e0bb26d3ca7e
mohamed <mr319242@gmail.com>
2023-05-16 23:16:27 +0300

    new

--- orders/prepare_order.php
+++ orders/prepare_order.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_GET['step_id'])) {
-    $step_id = $_GET['step_id']; 
+    $step_id = $_GET['step_id'];
     $stmt = $connect->prepare("SELECT * FROM order_steps WHERE id=?");
     $stmt->execute(array($step_id));
     $step_data = $stmt->fetch();
@@ -18,7 +18,6 @@ if (isset($_GET['step_id'])) {
     $order_data = $stmt->fetch();
     $payment_method = $order_data['payment_method'];
 }
-// تم التواصل بنجاح 
 if (isset($_POST['quality_rating'])) {
     $client_progress_note = $_POST['client_proceess_note'];
     $company_progress_note = $_POST['company_proceess_note'];
@@ -93,7 +92,111 @@ if (isset($_POST['quality_rating'])) {
         header('Location:main.php?dir=orders&page=order_details&order_id=' . $order_id);
         exit();
     }
-    // تم التواصل اكثر من ثلاث مرات ولاكن لم يتم الرد 
+}
+
+// اعادة تقيم الجودة
+
+if (isset($_POST['quality_rating_repeat'])) {
+    $order_step_detail_ids = $_POST['order_step_detail_id'];
+    for ($i = 0; $i < count($order_step_detail_ids); $i++) {
+        $step_detail_id = $order_step_detail_ids[$i];
+        if (isset($_FILES['product_images']['name'][$i])) {
+            $product_image_name = $_FILES['product_images']['name'][$i];
+            $product_image_temp = $_FILES['product_images']['tmp_name'][$i];
+            $product_image_type = $_FILES['product_images']['type'][$i];
+            $product_image_size = $_FILES['product_images']['size'][$i];
+            $product_image_uploaded = time() . '_' . $product_image_name;
+            move_uploaded_file(
+                $product_image_temp,
+                'steps_images/' . $product_image_uploaded
+            );
+        } else {
+            $product_image_uploaded = '';
+        }
+        // Check if $_FILES['product_images']['name'][$i] is set before executing the update statement
+        if ($_FILES['product_images']['name'][$i] != '') {
+            $stmt = $connect->prepare("UPDATE order_step_details SET product_image = ? WHERE id = ? ");
+            $stmt->execute(array($product_image_uploaded, $step_detail_id));
+        }
+    }
+    if ($stmt) {
+        // 1 - تحديد الموظف الخاص بالجودة
+        // 2 - تسجيل خطوة الجودة في خطوات الطلب 
+        $stmt = $connect->prepare("SELECT * FROM employes WHERE role_name='الجودة'");
+        $stmt->execute();
+        $emp_data = $stmt->fetch();
+        $emp_id = $emp_data['id'];
+        // تسجيل خطوة الجودة في خطوات تنفيذ الطلب 
+        $stmt = $connect->prepare("INSERT INTO order_steps (order_id , order_number, username ,date , step_name,step_status)
+        VALUES (:zorder_id,:zorder_number,:zusername,:zdate,:zstep_name,:zstatus)");
+        $stmt->execute(array(
+            "zorder_id" => $order_id,
+            "zorder_number" => $order_number,
+            "zusername" => $emp_id,
+            "zdate" => $date,
+            "zstep_name" => ' الجودة ',
+            "zstatus" => 'تم عمل ملاحظات الجودة وفي انتظار التاكيد ',
+        ));
+        // تحديث بيانات الطلب 
+        $stmt = $connect->prepare("UPDATE orders SET status_value=' انتظار مراجعه الجودة ' WHERE id=?");
+        $stmt->execute(array($order_id));
+        $_SESSION['success_message'] = " تمت التعديل  بنجاح  ";
+        header('Location:main.php?dir=orders&page=order_details&order_id=' . $order_id);
+    } else {
+        $_SESSION['error_messages'] = $formerror;
+        header('Location:main.php?dir=orders&page=order_details&order_id=' . $order_id);
+        exit();
+    }
+}
+
+// تم التجهيز بنجاح 
+
+if (isset($_POST['quality_compelete'])) {
+    $step_name = 'التوصيل';
+    $formerror = [];
+    if (empty($step_name)) {
+        $formerror[] = ' من فضلك حدد حالة التواصل ';
+    }
+    if (empty($formerror)) {
+        $stmt = $connect->prepare("INSERT INTO order_steps (order_id , order_number, username ,date , step_name,step_status)
+        VALUES (:zorder_id,:zorder_number,:zusername,:zdate,:zstep_name,:zstatus)");
+        $stmt->execute(array(
+            "zorder_id" => $order_id,
+            "zorder_number" => $order_number,
+            "zusername" => $username,
+            "zdate" => $date,
+            "zstep_name" => 'التوصيل',
+            "zstatus" => 'تم تجهيز الطلب والان في مرحلة التوصيل',
+        ));
+    }
+    if ($stmt) {
+        // 1 - تحديد الموظف الخاص بالجودة
+        // 2 - تسجيل خطوة الجودة في خطوات الطلب 
+        $stmt = $connect->prepare("SELECT * FROM employes WHERE role_name='التوصيل'");
+        $stmt->execute();
+        $emp_data = $stmt->fetch();
+        $emp_id = $emp_data['id'];
+        // تسجيل خطوة الجودة في خطوات تنفيذ الطلب 
+        $stmt = $connect->prepare("INSERT INTO order_steps (order_id , order_number, username ,date , step_name,step_status)
+        VALUES (:zorder_id,:zorder_number,:zusername,:zdate,:zstep_name,:zstatus)");
+        $stmt->execute(array(
+            "zorder_id" => $order_id,
+            "zorder_number" => $order_number,
+            "zusername" => $emp_id,
+            "zdate" => $date,
+            "zstep_name" => 'التوصيل',
+            "zstatus" => 'تم تجهيز الطلب والان في مرحلة التوصيل',
+        ));
+        // تحديث بيانات الطلب 
+        $stmt = $connect->prepare("UPDATE orders SET status_value='تم تجهيز الطلب والان في مرحلة التوصيل' WHERE id=?");
+        $stmt->execute(array($order_id));
+        $_SESSION['success_message'] = " تمت التعديل  بنجاح  ";
+        header('Location:main.php?dir=orders&page=order_details&order_id=' . $order_id);
+    } else {
+        $_SESSION['error_messages'] = $formerror;
+        header('Location:main.php?dir=orders&page=order_details&order_id=' . $order_id);
+        exit();
+    }
 }
 ?>
 <!-- Content Header (Page header) -->
@@ -117,15 +220,63 @@ if (isset($_POST['quality_rating'])) {
     <!-- /.container-fluid -->
 </section>
 <!-- /.content-header -->
+
 <!-- DOM/Jquery table start -->
 <section class="content">
     <div class="container-fluid">
-        <form action="" method="post" enctype="multipart/form-data" autocomplete="off">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card card-primary">
-                        <div class="card-body">
-                            <!-- <ul>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-primary">
+                    <div class="card-body">
+                        <!--- هل تم تجهيز الطلب من قبل وتحت المراجعه  -->
+                        <?php
+                        $stmt = $connect->prepare("SELECT * FROM order_steps WHERE step_name = 'الجودة' AND order_id = ?");
+                        $stmt->execute(array($order_id));
+                        $count = $stmt->rowCount();
+                        if ($count > 0) {
+                        ?>
+                            <form action="" method="post" enctype="multipart/form-data">
+                                <?php
+                                $stmt = $connect->prepare("SELECT * FROM order_step_details WHERE order_id = ?");
+                                $stmt->execute(array($order_id));
+                                $order_steps_data = $stmt->fetchAll();
+                                foreach ($order_steps_data as $step_data) {
+                                ?>
+                                    <div class="form-group">
+                                        <input type="hidden" name="order_step_detail_id[]" value="<?php echo $step_data['id']; ?>">
+                                        <label for=""> اسم المنتج :</label>
+                                        <input disabled type="text" name="product_name[]" class="form-control" value="<?php echo $step_data['product_name']; ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for=""> صورة المنتج لمعاينة الجودة :</label>
+                                        <a href="steps_images/<?php echo $step_data['product_image'] ?>" target="_blank"> <img style="width: 150px; height:150px" src="steps_images/<?php echo $step_data['product_image'] ?>" alt=""> </a>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for=""> تعديل صورة المنتج : </label>
+                                        <input type="file" name="product_images[]" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for=""> تقيم الجودة للمنتج :</label>
+                                        <span class="badge badge-danger"><?php echo $step_data['product_image_rating']; ?></span>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for=""> ملاحظة الجودة :</label>
+                                        <textarea disabled class="form-control"> <?php echo $step_data['product_notes']; ?> </textarea>
+                                    </div>
+                                    <hr>
+                                <?php
+                                }
+                                ?>
+                                <div class="row" style="display: flex;justify-content: space-between;">
+                                    <button type="submit" id="quality_rating" class="btn btn-warning quality_compelete btn-sm" name="quality_compelete"> اتمام تجهيز الطلب <i class="fa fa-save"></i> </button>
+                                    <button type="submit" id="quality_rating" class="btn btn-primary quality_rating btn-sm" name="quality_rating_repeat"> اعادة تقيم جودة <i class="fa fa-retweet"></i> </button>
+                                </div>
+                            </form>
+                        <?php
+                        } else {
+                        ?>
+                            <form action="" method="post" enctype="multipart/form-data" autocomplete="off">
+                                <!-- <ul>
                                 <h4> المتطلبات </h4>
                                 <li class="badge badge-warning"> 1- ارفاق صورة لكل منتج في الطلب </li>
                                 <li class="badge badge-warning"> 2- ملاحظات مجهز الطلب للعميل (اختياري) </li>
@@ -134,105 +285,52 @@ if (isset($_POST['quality_rating'])) {
                                 <li class="badge badge-warning"> 5- إثبات موافقة العميل على الصور كصورة مراسالات أو كتابة توضيح للإدارة بموافقة العميل شفوياً. </li>
                             </ul>
 -->
-                            <?php
-                            $stmt = $connect->prepare("SELECT * FROM order_details WHERE order_id = ?");
-                            $stmt->execute(array($order_id));
-                            $all_order_data = $stmt->fetchAll();
-                            foreach ($all_order_data as $order_data) {
-                                $stmt = $connect->prepare("SELECT * FROM products WHERE id = ?");
-                                $stmt->execute(array($order_data['product_id']));
-                                $product_data = $stmt->fetch();
-                            ?>
+                                <?php
+                                $stmt = $connect->prepare("SELECT * FROM order_details WHERE order_id = ?");
+                                $stmt->execute(array($order_id));
+                                $all_order_data = $stmt->fetchAll();
+                                foreach ($all_order_data as $order_data) {
+                                    $stmt = $connect->prepare("SELECT * FROM products WHERE id = ?");
+                                    $stmt->execute(array($order_data['product_id']));
+                                    $product_data = $stmt->fetch();
+                                ?>
+                                    <div class="form-group">
+                                        <label for=""> اسم المنتج </label>
+                                        <input type="text" name="product_name[]" class="form-control" value="<?php echo $product_data['name']; ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for=""> اختر صورة المنتج </label>
+                                        <input type="file" name="product_images[]" class="form-control">
+                                    </div>
+                                    <hr>
+                                <?php
+                                }
+                                ?>
                                 <div class="form-group">
-                                    <label for=""> اسم المنتج </label>
-                                    <input type="text" name="product_name[]" class="form-control" value="<?php echo $product_data['name']; ?>">
+                                    <label for="description"> ملاحظاتك للعميل :</label>
+                                    <textarea id="description" name="client_proceess_note" class="form-control" rows="4"><?php if (isset($_REQUEST['description'])) echo $_REQUEST['description'] ?></textarea>
                                 </div>
                                 <div class="form-group">
-                                    <label for=""> اختر صورة المنتج </label>
-                                    <input type="file" name="product_images[]" class="form-control">
+                                    <label for="description"> ملاحظاتك للإدارة :</label>
+                                    <textarea id="description" name="company_proceess_note" class="form-control" rows="4"><?php if (isset($_REQUEST['description'])) echo $_REQUEST['description'] ?></textarea>
                                 </div>
-                                <hr>
-                            <?php
-                            }
-                            ?>
-                            <div class="form-group">
-                                <label for="description"> ملاحظاتك للعميل :</label>
-                                <textarea id="description" name="client_proceess_note" class="form-control" rows="4"><?php if (isset($_REQUEST['description'])) echo $_REQUEST['description'] ?></textarea>
-                            </div>
-                            <div class="form-group">
-                                <label for="description"> ملاحظاتك للإدارة :</label>
-                                <textarea id="description" name="company_proceess_note" class="form-control" rows="4"><?php if (isset($_REQUEST['description'])) echo $_REQUEST['description'] ?></textarea>
-                            </div>
-                        </div>
-                        <!-- /.card-body -->
                     </div>
-                    <!-- /.card -->
+                    <!-- /.card-body -->
                 </div>
+                <!-- /.card -->
             </div>
-            <div class="row" style="display: flex;justify-content: space-between;">
-                <button type="submit" id="quality_rating" class="btn btn-primary quality_rating" name="quality_rating">ارسال لتقيم الجودة <i class="fa fa-save"></i> </button>
-                <!-- <a href="main.php?dir=products&page=report" class="btn btn-secondary">رجوع <i class="fa fa-backward"></i> </a> -->
-            </div>
+        </div>
+        <div class="row" style="display: flex;justify-content: space-between;">
+            <button type="submit" id="quality_rating" class="btn btn-primary quality_rating" name="quality_rating">ارسال لتقيم الجودة <i class="fa fa-save"></i> </button>
+            <!-- <a href="main.php?dir=products&page=report" class="btn btn-secondary">رجوع <i class="fa fa-backward"></i> </a> -->
+        </div>
         </form>
-        <br>
-        <br>
+    <?php
+                        }
+    ?>
+
+    <br>
+    <br>
     </div>
     <!-- /.container-fluid -->
 </section>
-
-<script>
-    // احضر العناصر اللازمة من الصفحة
-    /* const selectEl = document.getElementById('select2');
-     const dateEl = document.getElementById('arrival_date');
-     const arrival_step = document.getElementById('arrival_step');
-     const not_respose = document.getElementById('not_response');
-     const response_delay = document.getElementById('response_delay');
-     const arrival_delay = document.getElementById('arrival_delay');
-     const cancel_order = document.getElementById('cancel_order');
-     const return_file = document.getElementById('return_file');
-     const cancel_return = document.getElementById('cancel_return');
-     // أضف حدثًا onchange إلى القائمة المنسدلة
-     selectEl.onchange = function() {
-         const selectedValue = selectEl.value;
-         // فحص القيمة المحددة وإظهار / إخفاء الحقول الأخرى
-         if (selectedValue === 'تم التواصل') {
-             dateEl.style.display = 'block';
-             not_respose.style.display = 'none';
-             response_delay.style.display = 'none';
-         } else {
-             dateEl.style.display = 'none';
-         }
-         if (selectedValue === 'تواصل بدون رد') {
-             not_respose.style.display = 'block';
-             arrival_step.style.display = 'none';
-             response_delay.style.display = 'none';
-         }
-         if (selectedValue === 'تأجيل') {
-             response_delay.style.display = 'block';
-             arrival_delay.style.display = 'block';
-             not_respose.style.display = 'none';
-             arrival_step.style.display = 'none';
-         } else {
-             arrival_delay.style.display = 'none';
-         }
-         if (selectedValue === 'ملغي، يدويا') {
-             response_delay.style.display = 'none';
-             arrival_delay.style.display = 'none';
-             not_respose.style.display = 'none';
-             arrival_step.style.display = 'none';
-             cancel_order.style.display = 'block';
-         }
-         if (selectedValue === 'ملغي تم الاسترجاع') {
-             response_delay.style.display = 'none';
-             arrival_delay.style.display = 'none';
-             not_respose.style.display = 'none';
-             arrival_step.style.display = 'none';
-             cancel_order.style.display = 'none';
-             return_file.style.display = 'block';
-             cancel_return.style.display = 'block';
-         }
-     };*/
-</script>
-<style>
-
-</style>
