63fb428cf65b9831f0936c1300952455fd4f65fa
mohamed <mr319242@gmail.com>
2023-06-02 00:25:21 +0300

    new update

--- index.php
+++ index.php
@@ -16,25 +16,37 @@ include 'init.php';
                 aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
+
             <div class="carousel-item active">
+                <div class="overlay"></div>
                 <img src="uploads/header1.png" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
+                    <h5>العناية بالنباتات المنزلية 1 </h5>
+                    <p> يرغب في تمتعك بنباتات صحية ونامية، وأن تتوفق لأفضل المنتجات المناسبة لك ,لذلك وفرنا خبراء
+                        متخصصين بالهندسة الزراعية لتقديم الدعم والتوجيه، ولنساعدك في اختيار النباتات </p>
+                    <a href="#" class="btn"> تواصل مع الخبير </a>
+                </div>
+
+            </div>
+            <div class="carousel-item">
+                <div class="overlay"></div>
+                <img src="uploads/header1.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5> العناية بالنباتات المنزلية  2 </h5>
+                    <p>يرغب في تمتعك بنباتات صحية ونامية، وأن تتوفق لأفضل المنتجات المناسبة لك ,لذلك وفرنا خبراء متخصصين
+                        بالهندسة الزراعية لتقديم الدعم والتوجيه، ولنساعدك في اختيار
+                    </p>
+                    <a href="#" class="btn"> تواصل مع الخبير </a>
                 </div>
             </div>
             <div class="carousel-item">
+                <div class="overlay"></div>
                 <img src="uploads/header1.png" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Some representative placeholder content for the second slide.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="uploads/header1.png" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Some representative placeholder content for the third slide.</p>
+                    <h5> العناية بالنباتات المنزلية   3</h5>
+                    <p> يرغب في تمتعك بنباتات صحية ونامية، وأن تتوفق لأفضل المنتجات المناسبة لك ,لذلك وفرنا خبراء
+                        متخصصين بالهندسة الزراعية لتقديم الدعم والتوجيه، ولنساعدك في اختيار النباتات </p>
+                    <a href="#" class="btn"> تواصل مع الخبير </a>
                 </div>
             </div>
         </div>
