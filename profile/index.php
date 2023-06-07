fd580ab5155d130825619c6e4c6e21422aa2b7e2
mohamed <mr319242@gmail.com>
2023-06-07 12:45:39 +0300

    new update

--- profile/index.php
+++ profile/index.php
@@ -6,16 +6,126 @@ include 'init.php';
 ?>
 <div class="profile_page">
 
-<div class='container'>
-    <div class="data">
-        <div class="breadcrump">
-            <p> <a href="#"> الرئيسية   </a> / <span> حسابي  </span> </p>
+    <div class='container'>
+        <div class="data">
+            <div class="breadcrump">
+                <p> <a href="#"> الرئيسية </a> / <span> حسابي </span> </p>
+            </div>
+            <div class="data_header_name">
+                <h2 class='header2'> حسابي </h2>
+                <p> قم بتعديل بياناتك وتتبع طلباتك والمزيد </p>
+            </div>
+            <div class='row'>
+                <div class="col-lg-3">
+                    <a href="purchase">
+                        <div class="info">
+                            <div>
+                                <img src="<?php echo $uploads ?>/profile_payment.svg" alt="">
+                            </div>
+                            <div>
+                                <h3> مشترياتي </h3>
+                                <p> يمكنك تتبع جميع مشترياتك </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3">
+                    <a href="#">
+                        <div class="info">
+                            <div>
+                                <img src="<?php echo $uploads ?>/profile_payment.svg" alt="">
+                            </div>
+                            <div>
+                                <h3> عناويني </h3>
+                                <p> تحكم في عناوين الشحن الخاصه بك </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3">
+                    <a href="#">
+                        <div class="info">
+                            <div>
+                                <img src="<?php echo $uploads ?>/profile_payment.svg" alt="">
+                            </div>
+                            <div>
+                                <h3> الإرجاع </h3>
+                                <p> طلبات و سياسة إرجاع المنتجات </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3">
+                    <a href="#">
+                        <div class="info">
+                            <div>
+                                <img src="<?php echo $uploads ?>/profile_payment.svg" alt="">
+                            </div>
+                            <div>
+                                <h3> طرق الدفع </h3>
+                                <p> تحكم في طرق الدفع الخاصة بك </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3">
+                    <a href="#">
+                        <div class="info">
+                            <div>
+                                <img src="<?php echo $uploads ?>/profile_payment.svg" alt="">
+                            </div>
+                            <div>
+                                <h3> سلة الشراء </h3>
+                                <p> جميع المنتجات الموجودة بالسلة </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3">
+                    <a href="#">
+                        <div class="info">
+                            <div>
+                                <img src="<?php echo $uploads ?>/profile_payment.svg" alt="">
+                            </div>
+                            <div>
+                                <h3> المفضلة </h3>
+                                <p> منتجاتك المضافة للمفضلة </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3">
+                    <a href="#">
+                        <div class="info">
+                            <div>
+                                <img src="<?php echo $uploads ?>/profile_payment.svg" alt="">
+                            </div>
+                            <div>
+                                <h3> تغيير كلمة المرور </h3>
+                                <p> تغيير كلمة المرور الخاصة بك </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-3">
+                    <a href="#">
+                        <div class="info">
+                            <div>
+                                <img src="<?php echo $uploads ?>/profile_payment.svg" alt="">
+                            </div>
+                            <div>
+                                <h3> تعديل بياناتي </h3>
+                                <p> تعديل البيانات الشخصية </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 
 </div>
 <?php
-include $tem .'footer.php';
+include $tem . 'footer.php';
 ob_end_flush();
 ?>
---
+++ profile/purchase.php
@@ -0,0 +1,161 @@
+<?php
+ob_start();
+session_start();
+$page_title = ' حسابي  ';
+include 'init.php';
+?>
+<div class="profile_page">
+
+    <div class='container'>
+        <div class="data">
+            <div class="breadcrump">
+                <p> <a href="../index"> الرئيسية </a> \ <a href="index"> حسابي </a> \ <span> مشترياتي </span> </p>
+            </div>
+            <div class="purches_header">
+                <div class="data_header_name">
+                    <h2 class='header2'> مشترياتي </h2>
+                    <p> يمكنك تتبع جميع مشترياتك </p>
+                </div>
+                <div class='search'>
+                    <form action="#" method="post">
+                        <div class="input_box">
+                            <input type="text" placeholder="البحث برقم الطلب…" class='form-control'>
+                        </div>
+                        <div class="input_box">
+                            <button class="btn global_button" name="search_order" type='submit'> <img
+                                    src="<?php echo $uploads; ?>order_search.svg" alt=""> بحث </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class='purches_orders'>
+                <div class="card">
+                    <div class='card-header'>
+                        <div class='header_data'>
+                            <div class='order_num'>
+                                <h6> رقم الطلب :</h6>
+                                <p> 27550-221 </p>
+                            </div>
+                            <div class='order_num'>
+                                <h6> تم الطلب في : </h6>
+                                <p> 4 أبريل 2023 </p>
+                            </div>
+                            <div class='order_num'>
+                                <h6> تم الشحن الي : </h6>
+                                <p> مسكن الرحالة </p>
+                            </div>
+                        </div>
+                        <div class="print_section">
+                            <div class="total_invoice">
+                                <h6> أجمالي الفاتورة: </h6>
+                                <p> 280 ر. س </p>
+                            </div>
+                            <div class="total_invoice">
+                                <a href="#" class="btn global_button"> طباعة الفاتورة <img
+                                        src="<?php echo $uploads ?>print.svg" alt=""> </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='card-body'>
+                        <div class="body_data">
+                            <div class='product_data'>
+                                <div>
+                                    <img src="<?php echo $uploads ?>product.png" alt="">
+                                </div>
+                                <div>
+                                    <h2>شجيرة التيكوما</h2>
+                                    <span>87.00 ر.س</span>
+                                </div>
+                            </div>
+                            <div class='order_status'>
+                                <a href="#" class='btn global_button'> تتبع الطرد </a>
+                            </div>
+                        </div>
+                        <div class="body_data">
+                            <div class='product_data'>
+                                <div>
+                                    <img src="<?php echo $uploads ?>product.png" alt="">
+                                </div>
+                                <div>
+                                    <h2>شجيرة التيكوما</h2>
+                                    <span>87.00 ر.س</span>
+                                </div>
+                            </div>
+                            <div class='order_status'>
+                                <a href="#" class='btn global_button'> تتبع الطرد </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class='purches_orders'>
+                <div class="card">
+                    <div class='card-header'>
+                        <div class='header_data'>
+                            <div class='order_num'>
+                                <h6> رقم الطلب :</h6>
+                                <p> 27550-221 </p>
+                            </div>
+                            <div class='order_num'>
+                                <h6> تم الطلب في : </h6>
+                                <p> 4 أبريل 2023 </p>
+                            </div>
+                            <div class='order_num'>
+                                <h6> تم الشحن الي : </h6>
+                                <p> مسكن الرحالة </p>
+                            </div>
+                        </div>
+                        <div class="print_section">
+                            <div class="total_invoice">
+                                <h6> أجمالي الفاتورة: </h6>
+                                <p> 280 ر. س </p>
+                            </div>
+                            <div class="total_invoice">
+                                <a href="#" class="btn global_button"> طباعة الفاتورة <img
+                                        src="<?php echo $uploads ?>print.svg" alt=""> </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='card-body'>
+                        <div class="body_data">
+                            <div class='product_data'>
+                                <div>
+                                    <img src="<?php echo $uploads ?>product.png" alt="">
+                                </div>
+                                <div>
+                                    <h2>شجيرة التيكوما</h2>
+                                    <span>87.00 ر.س</span>
+                                </div>
+                            </div>
+                            <div class='order_status'>
+                                <p> تم التوصيل في: <span>5 أبريل 2023</span> </p>
+                            </div>
+                        </div>
+                        <div class="body_data">
+                            <div class='product_data'>
+                                <div>
+                                    <img src="<?php echo $uploads ?>product.png" alt="">
+                                </div>
+                                <div>
+                                    <h2>شجيرة التيكوما</h2>
+                                    <span>87.00 ر.س</span>
+                                </div>
+                            </div>
+                            <div class='order_status'>
+                                   <p> تم التوصيل في: <span>5 أبريل 2023</span> </p>
+                                <a href="#" class='btn global_button return_product'>
+                                    إرجاع المنتج </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php
+include $tem . 'footer.php';
+ob_end_flush();
+?>
