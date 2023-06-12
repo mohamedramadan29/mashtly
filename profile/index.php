98c8404982d954a0d4457130df5d7fc4948b6c63
mohamed <mr319242@gmail.com>
2023-06-12 18:08:35 +0300

    new update

--- profile/index.php
+++ profile/index.php
@@ -82,7 +82,7 @@ include 'init.php';
                     </a>
                 </div>
                 <div class="col-lg-3">
-                    <a href="#">
+                    <a href="favorite">
                         <div class="info">
                             <div>
                                 <img src="<?php echo $uploads ?>/profile_payment.svg" alt="">
