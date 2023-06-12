83b4d9afc33486b02b5df985cc92bb1e2037adb4
mohamed <mr319242@gmail.com>
2023-06-12 15:12:29 +0300

    new

--- cart.php
+++ cart.php
@@ -19,7 +19,7 @@ include 'init.php';
             </div>
             <div class="cart">
                 <div class="row">
-                    <div class="col-lg-9">
+                    <div class="col-lg-8">
                         <div class="card_items">
                             <span class="fa fa-close remove_item"> </span>
                             <div class="product_data">
@@ -29,7 +29,7 @@ include 'init.php';
                                 <div class="product_info">
                                     <h3> نبات ملكة النهار </h3>
                                     <p class="item_price"> سعر الوحدة :<span> 87.00 ر.س </span> </p>
-                                    <p class="add_fav"> <img src="<?php echo $uploads ?>heart.png" alt=""> أضف الي المفضلة  </p>
+                                    <p class="add_fav"> <img src="<?php echo $uploads ?>heart.png" alt=""> أضف الي المفضلة </p>
                                 </div>
                             </div>
                             <div class="product_num">
@@ -62,6 +62,156 @@ include 'init.php';
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                        <div class="card_items">
+                            <span class="fa fa-close remove_item"> </span>
+                            <div class="product_data">
+                                <div class="product_image">
+                                    <img src="<?php echo $uploads ?>product.png" alt="">
+                                </div>
+                                <div class="product_info">
+                                    <h3> نبات ملكة النهار </h3>
+                                    <p class="item_price"> سعر الوحدة :<span> 87.00 ر.س </span> </p>
+                                    <p class="add_fav"> <img src="<?php echo $uploads ?>heart.png" alt=""> أضف الي المفضلة </p>
+                                </div>
+                            </div>
+                            <div class="product_num">
+                                <form action="#" method="post">
+                                    <div class="counter">
+                                        <span class="plus" id="fa-plus"> <img src="<?php echo $uploads ?>plus.svg" alt=""> </span>
+                                        <span class="count_number" id="count_number"> 1 </span>
+                                        <span class="minus" id="fa-minus"> <img src="<?php echo $uploads ?>minus.svg" alt=""> </span>
+                                    </div>
+                                </form>
+                                <p> إجمالي السعر: <span> 87.00 ر.س </span> </p>
+                            </div>
+                            <div class="services">
+                                <form action="#" method="post">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            أضف خدمة الزراعة
+                                        </label>
+                                    </div>
+                                    <p> <span> 15 ر.س </span> <a href="#"> إعرف أكثر عن التكلفة </a> </p>
+                                </form>
+                                <div class="gift">
+                                    <div class="image">
+                                        <img src="<?php echo $uploads ?>product.png" alt="">
+                                    </div>
+                                    <div class="gift_info">
+                                        <h3> التغليف كهدية </h3>
+                                        <p> يتم إضافة 20 ر.س </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card_items">
+                            <span class="fa fa-close remove_item"> </span>
+                            <div class="product_data">
+                                <div class="product_image">
+                                    <img src="<?php echo $uploads ?>product.png" alt="">
+                                </div>
+                                <div class="product_info">
+                                    <h3> نبات ملكة النهار </h3>
+                                    <p class="item_price"> سعر الوحدة :<span> 87.00 ر.س </span> </p>
+                                    <p class="add_fav"> <img src="<?php echo $uploads ?>heart.png" alt=""> أضف الي المفضلة </p>
+                                </div>
+                            </div>
+                            <div class="product_num">
+                                <form action="#" method="post">
+                                    <div class="counter">
+                                        <span class="plus" id="fa-plus"> <img src="<?php echo $uploads ?>plus.svg" alt=""> </span>
+                                        <span class="count_number" id="count_number"> 1 </span>
+                                        <span class="minus" id="fa-minus"> <img src="<?php echo $uploads ?>minus.svg" alt=""> </span>
+                                    </div>
+                                </form>
+                                <p> إجمالي السعر: <span> 87.00 ر.س </span> </p>
+                            </div>
+                            <div class="services">
+                                <form action="#" method="post">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                        <label class="form-check-label" for="flexCheckChecked">
+                                            أضف خدمة الزراعة
+                                        </label>
+                                    </div>
+                                    <p> <span> 15 ر.س </span> <a href="#"> إعرف أكثر عن التكلفة </a> </p>
+                                </form>
+                                <div class="gift">
+                                    <div class="image">
+                                        <img src="<?php echo $uploads ?>product.png" alt="">
+                                    </div>
+                                    <div class="gift_info">
+                                        <h3> التغليف كهدية </h3>
+                                        <p> يتم إضافة 20 ر.س </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="cart_price_info">
+                            <p class="no_sheap_price">
+                                <img src="<?php echo $uploads ?>free.svg" alt="">
+                                أضف 13 ريال واحصل علي شحن مجاني
+
+                            </p>
+                            <div class="price_sections">
+                                <div class="first">
+                                    <div>
+                                        <h3> المجموع الفرعي: </h3>
+                                        <p> إجمالي سعر المنتجات في السلة </p>
+                                    </div>
+                                    <div>
+                                        <h2 class="total"> 87.00 ر.س </h2>
+                                    </div>
+
+                                </div>
+                                <div class="first">
+                                    <div>
+                                        <h3> تكلفة الإضافات: </h3>
+                                        <p> تكلفة الزراعة + تكلفة التغليف كهدية </p>
+                                    </div>
+                                    <div>
+                                        <h2 class="total"> 87.00 ر.س </h2>
+                                    </div>
+
+                                </div>
+                                <div class="first">
+                                    <div>
+                                        <h3> الشحن والتسليم: </h3>
+                                        <p> يحدد سعر الشحن حسب الموقع </p>
+                                    </div>
+                                    <div>
+                                        <h2 class="total"> 87.00 ر.س </h2>
+                                    </div>
+
+                                </div>
+                                <div class="first">
+                                    <div>
+                                        <h3> ضريبة القيمة المضافة VAT: </h3>
+                                        <p> القيمة المضافة تساوي 15% من اجمالي الطلب </p>
+                                    </div>
+                                    <div>
+                                        <h2 class="total"> 87.00 ر.س </h2>
+                                    </div>
+
+                                </div>
+                                <hr>
+                                <div class="first">
+                                    <div>
+                                        <h3> إجمالي المبلغ: </h3>
+                                        <p> المبلغ المطلوب دفعه </p>
+                                    </div>
+                                    <div>
+                                        <h2 class="total"> 87.00 ر.س </h2>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <a href="#" class="btn global_button"> تابع عملية الشراء </a>
+
                         </div>
                     </div>
                 </div>
