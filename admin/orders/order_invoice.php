b7d6cd68d5dc3232c2be5e685ba08f9e95c1c33d
mohamed <mr319242@gmail.com>
2023-09-16 21:11:52 +0300

    update

--- admin/orders/order_invoice.php
+++ admin/orders/order_invoice.php
@@ -19,6 +19,9 @@ if (isset($_GET['order_id'])) {
     $stmt = $connect->prepare("SELECT * FROM orders WHERE id = ?");
     $stmt->execute(array($order_id));
     $order_data = $stmt->fetch();
+    $ship_price = $order_data['ship_price'];
+    $order_date = $order_data['order_date'];
+    $total_price = $order_data['total_price'];
 }
 ?>
 <!-- Content Header (Page header) -->
@@ -57,8 +60,8 @@ if (isset($_GET['order_id'])) {
                             <div class="header_data">
                                 <div class="row" style=" padding:10px; margin-top:10px; margin-bottom:10px">
                                     <h2> مرحبا <?php echo $order_data['name']; ?> </h2>
-                                    <p> شكرا لطلبك، من مشتلي تم تأكيد طلبك وسوف يصلك في الوقت المحدد لإلغاء الطلب أو تعديله يرجي زيارة الموقع قسم مشترياتي </p>
-                                    <p> يوجد أدناه فاتورة برقم الطلب وتفاصيله </p>
+                                    <p> شكرا لطلبك، من مشتلي تم تأكيد طلبك وسوف يصلك في الوقت المحدد لإلغاء الطلب أو تعديله يرجي زيارة الموقع قسم <span style="color: #7a9d12;">مشترياتي</span> </p>
+                                    <p style="display: block;"> يوجد أدناه فاتورة برقم الطلب وتفاصيله </p>
                                     <div class="d-flex ul_div">
                                         <ul class="list-unstyled">
                                             <li> <span> رقم الطلب: </span> </li>
@@ -78,66 +81,120 @@ if (isset($_GET['order_id'])) {
                                 </div>
                             </div>
                             <div class="order_details">
-                                <h3 style="font-size: 20px; color:#7a9d12;margin-top:15px;"> تفاصيل الطلب </h3>
-                                <table class="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th> المنتج </th>
-                                            <th> صورة المنتج </th>
-                                            <th> الكمية </th>
-                                            <th> السعر </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        $stmt = $connect->prepare("SELECT * FROM order_details WHERE order_id = ?");
-                                        $stmt->execute(array($order_id));
-                                        $allorder_details = $stmt->fetchAll();
-                                        foreach ($allorder_details as $order_detail) {
-                                        ?>
-                                            <tr>
-                                                <td>
-                                                    <?php
-                                                    $stmt = $connect->prepare("SELECT * FROM products WHERE id = ?");
-                                                    $stmt->execute(array($order_detail['product_id']));
-                                                    $product_data = $stmt->fetch();
-                                                    echo $product_data['name'];
-                                                    ?>
-                                                </td>
-                                                <td>
-                                                    <?php if (!empty($product_data['main_image']) && strpos($product_data['main_image'], "https://www.mshtly.com") !== false) { ?>
-                                                        <img style="width: 80px; height:80px;" src="<?php echo $product_data['main_image']; ?>" alt="">
-                                                    <?php } elseif (!empty($product_data['main_image'])) { ?>
-                                                        <img style="width: 80px; height:80px;" src="product_images/<?php echo $product_data['main_image']; ?>" alt="">
-                                                    <?php } ?>
-                                                </td>
-                                                <td><?php echo $order_detail['qty']; ?> </td>
-                                                <td><?php echo $order_detail['total']; ?> ر.س </td>
-                                            </tr>
-                                        <?php
-                                        }
-                                        ?>
-                                    </tbody>
-                                </table>
-                                <table class="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th> تكلفه الشحن </th>
-                                            <th> اجمالي الفاتورة </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td> <?php echo  $order_data['ship_price'] ?> ر.س </td>
-                                            <td class="text-bold"> <?php echo  $order_data['total_price'] ?> ر.س </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                <h4> تفاصيل الطلب </h4>
+                                <div class="order">
+                                    <div>
+                                        <h6> المنتج </h6>
+                                    </div>
+                                    <div style="display: flex;justify-content: space-between;min-width: 40%;">
+                                        <div>
+                                            <h6> الكمية </h6>
+                                        </div>
+                                        <div>
+                                            <h6> إجمالي السعر </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php
+                                $stmt = $connect->prepare("SELECT * FROM order_details WHERE order_id = ?");
+                                $stmt->execute(array($order_id));
+                                $alldetails = $stmt->fetchAll();
+                                foreach ($alldetails as $details) {
+                                    // get product data
+                                    $stmt = $connect->prepare("SELECT * FROM products WHERE id = ?");
+                                    $stmt->execute(array($details['product_id']));
+                                    $product_data = $stmt->fetch();
+                                    // get the product image
+                                    $stmt = $connect->prepare("SELECT * FROM products_image WHERE product_id = ?");
+                                    $stmt->execute(array($product_data['id']));
+                                    $product_image = $stmt->fetch();
+                                    $image = $product_image['main_image'];
+                                ?>
+                                    <div class="order" style="display: flex;">
+                                        <div>
+                                            <div class="product_data">
+                                                <div class="image">
+                                                    <img src="product_images/<?php echo $image ?>" alt="">
+                                                </div>
+                                                <div>
+                                                    <h4> <?php echo $product_data['name']; ?> </h4>
+                                                    <p> <?php echo number_format($product_data['price'], 2); ?> ر.س </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div style="display: flex;justify-content: space-between;min-width: 30%;">
+                                            <div>
+                                                <span> <?php echo $details['qty']; ?> </span>
+                                            </div>
+                                            <div>
+                                                <span> <?php echo  number_format($details['qty'] * $details['total'], 2); ?> ر.س </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php
+                                }
+
+                                ?>
+                            </div>
+                            <div class="order_totals">
+                                <div class="price_sections">
+                                    <div class="first">
+                                        <div>
+                                            <h3> المجموع الفرعي: </h3>
+                                            <p> إجمالي سعر المنتجات في السلة </p>
+                                        </div>
+                                        <div>
+                                            <h2 class="total"> <?php echo number_format(50, 2); ?> ر.س </h2>
+                                        </div>
+                                    </div>
+                                    <div class="first">
+                                        <div>
+                                            <h3> تكلفة الإضافات: </h3>
+                                            <p> تكلفة الزراعة + تكلفة التغليف كهدية </p>
+                                        </div>
+                                        <div>
+                                            <h2 class="total"> <?php echo number_format(40, 2); ?> ر.س </h2>
+                                        </div>
+                                    </div>
+                                    <div class="first">
+                                        <div>
+                                            <h3> الشحن والتسليم: </h3>
+                                            <p> يحدد سعر الشحن حسب الموقع </p>
+                                        </div>
+                                        <div>
+                                            <h2 class="total"><?php echo number_format($ship_price, 2); ?> ر.س </h2>
+                                        </div>
+                                    </div>
+                                    <div class="first">
+                                        <div>
+                                            <h3> ضريبة القيمة المضافة VAT: </h3>
+                                            <p> القيمة المضافة تساوي 15% من اجمالي الطلب </p>
+                                        </div>
+                                        <div>
+                                            <?php
+                                            $vat = $total_price * (15 / 100);
+
+                                            ?>
+                                            <h2 class="total"> <?php echo number_format($vat, 2); ?> ر.س </h2>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="first">
+                                        <div>
+                                            <h3> إجمالي المبلغ: </h3>
+                                            <p> المبلغ المطلوب دفعه </p>
+                                        </div>
+                                        <div>
+                                            <h2 class="total"> <?php echo number_format($total_price, 2); ?> ر.س </h2>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p class="thanks"> شكرا لتسوقكم من <a href="#"> مشتلي </a> </p>
                             </div>
 
                         </div>
                         <div class="row" style="display: flex;justify-content: space-between;">
-                            <button class="btn btn-primary btn-sm" id="print_Button" onclick="printDiv()"> <i class="fa fa-print"></i> طباعه الفاتورة </button>
+                            <button id="print_Button" onclick="window.print(); return false;" class="global_button btn print-link btn-primary"> طباعة الفاتورة <i class="fa fa-print"></i> </button>
                         </div>
                     </div>
                 </div>
@@ -148,23 +205,38 @@ if (isset($_GET['order_id'])) {
 </section>
 <style>
     @media print {
+
+        .footer,
+        .bottom_footer,
+        .main_navbar,
+        .instagrame_footer,
+        .main-sidebar {
+            display: none !important;
+        }
+
+        .print_order {
+            max-width: 100% !important;
+            padding: 10px !important;
+        }
+
         body {
-            -webkit-print-color-adjust: exact !important;
-            print-color-adjust: exact !important;
+            background-color: #fff;
         }
 
         #print_Button {
             display: none !important;
         }
+
+        .print-link {
+            display: none !important;
+        }
+
+        @page {
+            margin: 0;
+        }
+
+        body {
+            margin: 1.6cm;
+        }
     }
 </style>
-<script type="text/javascript">
-    function printDiv() {
-        var printContents = document.getElementById('print').innerHTML;
-        var originalContents = document.body.innerHTML;
-        document.body.innerHTML = printContents;
-        window.print();
-        document.body.innerHTML = originalContents;
-        location.reload();
-    }
-</script>
