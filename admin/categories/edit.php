12ececd38f93fa4038a7335dda13dbe95ac7afa6
mohamed <mr319242@gmail.com>
2024-03-05 23:41:45 +0200

    update

--- admin/categories/edit.php
+++ admin/categories/edit.php
@@ -23,23 +23,6 @@ if (isset($_POST['edit_cat'])) {
         $main_image_uploaded = time() . '_' . $main_image_name;
         $upload_path = 'category_images/' . $main_image_uploaded;
         move_uploaded_file($main_image_temp, $upload_path);
-        // Check the image type and convert it to WebP if it's supported
-        if (exif_imagetype($upload_path) === IMAGETYPE_JPEG) {
-            $image = imagecreatefromjpeg($upload_path);
-        } elseif (exif_imagetype($upload_path) === IMAGETYPE_PNG) {
-            $image = imagecreatefrompng($upload_path);
-        }
-        if ($image !== false) {
-            $webp_path = 'category_images/' . pathinfo($main_image_uploaded, PATHINFO_FILENAME) . '.webp';
-            // Save the image as WebP
-            imagewebp($image, $webp_path);
-            // Clean up memory
-            imagedestroy($image);
-            // Update the uploaded image path to the WebP version
-            $main_image_uploaded = pathinfo($main_image_uploaded, PATHINFO_FILENAME) . '.webp';
-        }
-    } else {
-        $main_image_uploaded = '';
     }
 
     $stmt = $connect->prepare("SELECT * FROM categories WHERE slug = ?");
--- admin/orders/delete.php
+++ admin/orders/delete.php
@@ -1,36 +1,16 @@
 <?php
-if (isset($_GET['pro_id']) && is_numeric($_GET['pro_id'])) {
-    $pro_id = $_GET['pro_id'];
+if (isset($_GET['order_id']) && is_numeric($_GET['order_id'])) {
+    $order_id = $_GET['order_id'];
 
-    $stmt = $connect->prepare('SELECT * FROM products WHERE id= ?');
-    $stmt->execute([$pro_id]);
-    $pro_data = $stmt->fetch();
-    $pro_image = $pro_data['main_image'];
-    $more_images = $pro_data['more_images'];
-
-    if (file_exists($pro_image)) {
-        $product_image = "product_images/" . $pro_image;
-
-        unlink($product_image);
-    }
-    // Delete More images 
-    if (!empty($more_images)) {
-        $image_arr = explode(',', $more_images);
-        foreach ($image_arr as $image) {
-            $image_file = "product_images/" . $image;
-            if (file_exists($image_file)) {
-                unlink($image_file);
-            }
-        }
-    }
-    $count = $stmt->rowCount();
-    if ($count > 0) {
-
-        $stmt = $connect->prepare('DELETE FROM products WHERE id=?');
-        $stmt->execute([$pro_id]);
-        if ($stmt) {
-            $_SESSION['success_message'] = " تم الحذف بنجاح  ";
-            header('Location:main?dir=products&page=report');
-        }
+    $stmt = $connect->prepare('SELECT * FROM orders WHERE id= ?');
+    $stmt->execute([$order_id]);
+    $order_data = $stmt->fetch();
+    $stmt = $connect->prepare("DELETE FROM order_details WHERE order_id = ?");
+    $stmt->execute(array($order_id));
+    $stmt = $connect->prepare('DELETE FROM orders WHERE id=?');
+    $stmt->execute([$order_id]);
+    if ($stmt) {
+        $_SESSION['success_message'] = " تم الحذف بنجاح  ";
+        header('Location:main?dir=orders&page=report');
     }
 }
